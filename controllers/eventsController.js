ecfd155f38862c59e95d8f28cdf6ac2f4dcafe24
1804470738 <mauriciomatango_@hotmail.com>
2022-04-15 11:37:20 -0500

    finished backend

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,38 +1,148 @@
 const { response } = require('express');
+const EventoModel = require('../models/EventoModel');
+
 
 
 const getEventos = async (req, res = response) => {
-    res.json({
+
+    const eventos = await EventoModel.find().populate('user', 'name');
+
+    res.status(200).json({
+        eventos,
         ok: true,
-        msg: 'Todos los eventos traidos'
+        msg: 'Consulta exitosa',
     });
+};
+
+
+const crearEvento = async (req, res = response) => {
+
+    const eventInput = req.body;
+    const evento = new EventoModel(eventInput);
+
+    try {
+
+        evento.user = req.uid;
+
+        const newEvent = await evento.save();
+
+        res.status(201).json({
+            evento: newEvent,
+            msg: 'Evento creado',
+            ok: true,
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+};
+
+const acutalizarEvento = async (req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await EventoModel.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado'
+            });
+        }
+
+        if (evento.user.toString() !== uid) {
+
+            return res.status(401).json({
+                ok: false,
+                msg: 'No esta autorizado para actualizar este evento'
+            });
+        }
+
+
+        const newEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await EventoModel.findOneAndUpdate(eventoId, newEvento, { new: true });
+
+        res.status(200).json({
+            evento: eventoActualizado,
+            ok: true,
+            msg: 'Evento actualizado',
+        });
+
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+
+};
+
+const deleteEvento = async (req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await EventoModel.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado'
+            });
+        }
+
+        if (evento.user.toString() !== uid) {
+
+            return res.status(401).json({
+                ok: false,
+                msg: 'No esta autorizado para eliminar este evento'
+            });
+        }
+
+
+        await EventoModel.findByIdAndDelete(eventoId);
+
+        res.status(200).json({
+            ok: true,
+            msg: 'Evento eliminado exitosamente',
+        });
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
 };
 
 
 
 
-const crearEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Evento creado'
-    });
-};
-
-const acutalizarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Evento actualizado'
-    });
-};
-
-const deleteEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Evento eliminado'
-    });
-};
-
-module.exports ={
+module.exports = {
     getEventos,
     crearEvento,
     acutalizarEvento,
--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validate-campos');
 const { getEventos, getEvento, crearEvento, acutalizarEvento, deleteEvento } = require('../controllers/eventsController');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -12,10 +15,26 @@ router.use(validarJWT); //le digo que todas las rutas tengan validacion de token
 router.get('/', getEventos);
 
 
-router.post('/', crearEvento);
+router.post('/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalizacion es obligatoria').custom(isDate),
+    ],
+    validarCampos,
+    crearEvento
+);
 
 
-router.put('/:id', acutalizarEvento);
+router.put('/:id',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalizacion es obligatoria').custom(isDate),
+    ],
+    validarCampos,
+    acutalizarEvento
+);
 
 
 router.delete('/:id', deleteEvento);
