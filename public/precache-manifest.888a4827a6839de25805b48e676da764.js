11a1ca32e13c57e969a0af619238819ca7936921
1804470738 <mauriciomatango_@hotmail.com>
2022-04-17 14:02:22 -0500

    v1 Calendar App

---
+++ public/precache-manifest.888a4827a6839de25805b48e676da764.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3dc354ec1d0994d15eab32101a94d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ca5fb8c77c710d664fe",
+    "url": "/static/css/2.6a846250.chunk.css"
+  },
+  {
+    "revision": "049458844bc273981320",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "9ca5fb8c77c710d664fe",
+    "url": "/static/js/2.361826c6.chunk.js"
+  },
+  {
+    "revision": "5d8c8be002cd0ed21fa2e0cc704f2235",
+    "url": "/static/js/2.361826c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049458844bc273981320",
+    "url": "/static/js/main.6dc38082.chunk.js"
+  },
+  {
+    "revision": "3365454410a9ca82f300",
+    "url": "/static/js/runtime-main.29165fbf.js"
+  }
+]);
